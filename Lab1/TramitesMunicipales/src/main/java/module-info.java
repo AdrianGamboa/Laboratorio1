4ee6b732e527b344fe6705d2cfe1d5ca339c4019
Adrian Gamboa Delgado <norman.gamboa.delgado@est.una.ac.cr>
2020-09-14 21:25:49 -0600

    Diseños, dashboard, departamentos y login

--- Lab1/TramitesMunicipales/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.una.tramitesmunicipales {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.una.tramitesmunicipales to javafx.fxml;
-    exports org.una.tramitesmunicipales;
-}
